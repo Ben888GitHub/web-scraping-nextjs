df80d81b70ef38fe89feebb5471f92b697f6781e
Benedict <benedictryan80@gmail.com>
2023-07-23 21:22:11 +0700

    1st commit

---
+++ pages/api/scrape-with-cheerio/[username].js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { load } from 'cheerio';
+
+const handler = async (req, res) => {
+	const { username } = req.query;
+
+	const { data } = await axios.get(`https://www.instagram.com/${username}/`);
+
+	const $ = load(data);
+	const scriptTags = $('script[type="application/ld+json"]');
+	const jsonData = JSON.parse(scriptTags.html());
+
+	res.status(200).json(jsonData);
+};
+
+export default handler;
