10e13345d6c58f6e58fb48e5f3c8aaac8dc2b388
Benedict <benedictryan80@gmail.com>
2023-07-25 15:04:10 +0700

    change sth on api route

--- pages/api/scrape-with-cheerio.js
+++ pages/api/scrape-with-cheerio.js
@@ -2,20 +2,25 @@ import axios from 'axios';
 import { load } from 'cheerio';
 
 const handler = async (req, res) => {
-	try {
-		const { username } = req.body;
+	const { username } = req.body;
 
-		const { data } = await axios.get(`https://www.instagram.com/${username}/`);
+	console.log(username);
+	try {
+		const { data } = await axios.get(
+			`https://www.instagram.com/${req.body.username}/`
+		);
 
 		const $ = load(data);
 		const scriptTags = $('script[type="application/ld+json"]');
 		const jsonData = JSON.parse(scriptTags.html());
 
+		console.log(jsonData);
 		res.status(200).json(jsonData);
 	} catch (err) {
 		console.error('Error fetching Instagram profile:', error.message);
 		res.status(500).json({ error: 'Error fetching Instagram profile' });
 	}
+	// res.status(200).json({ username: username });
 };
 
 export default handler;
