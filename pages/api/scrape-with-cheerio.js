4d590e62e03399b48bd4ac2ed8dd7161098976e4
Benedict <benedictryan80@gmail.com>
2023-07-25 14:54:22 +0700

    use try catch in api route

--- pages/api/scrape-with-cheerio.js
+++ pages/api/scrape-with-cheerio.js
@@ -2,15 +2,20 @@ import axios from 'axios';
 import { load } from 'cheerio';
 
 const handler = async (req, res) => {
-	const { username } = req.body;
+	try {
+		const { username } = req.body;
 
-	const { data } = await axios.get(`https://www.instagram.com/${username}/`);
+		const { data } = await axios.get(`https://www.instagram.com/${username}/`);
 
-	const $ = load(data);
-	const scriptTags = $('script[type="application/ld+json"]');
-	const jsonData = JSON.parse(scriptTags.html());
+		const $ = load(data);
+		const scriptTags = $('script[type="application/ld+json"]');
+		const jsonData = JSON.parse(scriptTags.html());
 
-	res.status(200).json(jsonData);
+		res.status(200).json(jsonData);
+	} catch (err) {
+		console.error('Error fetching Instagram profile:', error.message);
+		res.status(500).json({ error: 'Error fetching Instagram profile' });
+	}
 };
 
 export default handler;
