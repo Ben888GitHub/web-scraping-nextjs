562d2237fcd04acd0f642fd2451ce2e1b4d5aaa2
Benedict <benedictryan80@gmail.com>
2023-07-25 16:19:41 +0700

    let see

--- pages/api/get-ig-profile.js
+++ pages/api/get-ig-profile.js
@@ -9,7 +9,13 @@ const handler = async (req, res) => {
 		console.log(username);
 		try {
 			const { data } = await axios.get(
-				`https://www.instagram.com/${req.body.username}/`
+				`https://www.instagram.com/${req.body.username}/`,
+				{
+					headers: {
+						'User-Agent':
+							'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+					}
+				}
 			);
 			// console.log(data);
 			const $ = await load(data);
